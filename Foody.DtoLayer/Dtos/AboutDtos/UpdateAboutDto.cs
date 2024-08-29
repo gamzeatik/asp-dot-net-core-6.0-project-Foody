1e69fcb4602a32d207beed69ac3cac688022b2b8
gamzeatik <gamzeatik33@hotmail.com>
2024-08-29 14:02:00 +0300

    added dto layer

---
+++ Foody.DtoLayer/Dtos/AboutDtos/UpdateAboutDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DtoLayer.Dtos.AboutDtos
+{
+    public class UpdateAboutDto
+    {
+        public int AboutId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Foody.DtoLayer/Dtos/SliderDtos/GetByIdSliderDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DtoLayer.Dtos.SliderDtos
+{
+    public class GetByIdSliderDto
+    {
+        public int SliderId { get; set; }
+        public string SliderTitle { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
